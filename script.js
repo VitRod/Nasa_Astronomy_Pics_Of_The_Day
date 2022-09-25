ef6c2f7c45dd34677647f03f7db14c441328f6f6
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-25 12:26:19 +0300

    const resultsNav = document.getElementById('resultsNav'); const favoritesNav = document.getElementById('favoritesNav'); const imagesContainer = document.querySelector('.images-container'); const saveConfirmed = document.querySelector('.save-confirmed'); const loader = document.querySelector('.loader');
    // NASA DEMO API
    const count = 10;
    const apiKey = 'DEMO_KEY';
    const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
    
    let resultsArray = [];
    let favorites = {};

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const resultsNav = document.getElementById('resultsNav');
+const favoritesNav = document.getElementById('favoritesNav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
+// NASA DEMO API
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+let favorites = {};
